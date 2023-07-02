b890cf3f72514b26aad2ee837872cf7d09afabc9
lalithasaga <132065591+lalithasaga@users.noreply.github.com>
2023-07-02 20:11:17 +0530

    Update Emailrow.js

--- Emailrow.js
+++ Emailrow.js
@@ -1,161 +1,5 @@
-/*import {  IconButton } from "@material-ui/core";
-import React from "react";
-import "./EmailRow.css";
-import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
-import { useDispatch } from "react-redux";
-import { selectMail } from "./Redux/MailSlice";
-import { useNavigate } from "react-router-dom";
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-
-function EmailRow({ id, title1, subject, description, time }) {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const openMail = () => {
-    dispatch(
-      selectMail({
-        id,
-        title1,
-        subject,
-        description,
-        time,
-      })
-    );
-    navigate("/mail");
-  };
-
-  return (
-    <div onClick={openMail} className="emailRow">
-      <div className="emailRow-options">
-        <IconButton>
-          <LabelImportantOutlinedIcon />
-        </IconButton>
-        <IconButton>
-          <FiberManualRecordIcon/>
-        </IconButton>
-      </div>
-      <h3 className="emailRow-title">{title1}</h3>
-      <div className="emailRow-message">
-        <h4>
-          {subject} <span className="emailRow-description"> - {description}</span>
-        </h4>
-      </div>
-      <p className="emailRow-time">{time}</p>
-    </div>
-  );
-}
-
-export default EmailRow; */
-
-/*import { IconButton } from "@material-ui/core";
-import React from "react";
-import "./EmailRow.css";
-import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
-import { useDispatch } from "react-redux";
-import { selectMail } from "./Redux/MailSlice";
-import { useNavigate } from "react-router-dom";
-import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
-
-function EmailRow({ id, title1, subject, description, time }) {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const openMail = () => {
-    dispatch(
-      selectMail({
-        id,
-        title1,
-        subject,
-        description,
-        time,
-      })
-    );
-    navigate("/mail");
-  };
-
-  return (
-    <div onClick={openMail} className="emailRow">
-      <div className="emailRow-options">
-        <IconButton>
-          <LabelImportantOutlinedIcon />
-        </IconButton>
-        <IconButton>
-          <FiberManualRecordIcon style={{ color: "blue", fontSize: "8px" }} />
-        </IconButton>
-      </div>
-      <h3 className="emailRow-title">{title1}</h3>
-      <div className="emailRow-message">
-        <h4>
-          {subject}{" "}
-          <span className="emailRow-description"> - {description}</span>
-        </h4>
-      </div>
-      <p className="emailRow-time">{time}</p>
-    </div>
-  );
-}
-
-export default EmailRow; */
-
-
-/*import { IconButton } from "@material-ui/core";
-import React, { useState } from "react";
-import "./EmailRow.css";
-import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
-import { useDispatch } from "react-redux";
-import { selectMail } from "./Redux/MailSlice";
-import { useNavigate } from "react-router-dom";
-import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
-
-function EmailRow({ id, title1, subject, description, time }) {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [isRead, setIsRead] = useState(false);
-
-  const openMail = () => {
-    setIsRead(true);
-    dispatch(
-      selectMail({
-        id,
-        title1,
-        subject,
-        description,
-        time,
-      })
-    );
-    navigate("/mail");
-  };
-
-  return (
-    <div onClick={openMail} className={`emailRow ${isRead ? "read" : "unread"}`}>
-      <div className="emailRow-options">
-        <IconButton>
-          <LabelImportantOutlinedIcon />
-        </IconButton>
-        {!isRead && (
-          <IconButton>
-            <FiberManualRecordIcon
-              style={{ color: "blue", fontSize: "8px" }}
-            />
-          </IconButton>
-        )}
-      </div>
-      <h3 className="emailRow-title">{title1}</h3>
-      <div className="emailRow-message">
-        <h4>
-          {subject}{" "}
-          <span className="emailRow-description"> - {description}</span>
-        </h4>
-      </div>
-      <p className="emailRow-time">{time}</p>
-    </div>
-  );
-}
-
-export default EmailRow; */
-
 import { IconButton } from "@material-ui/core";
-import React from "react";
+import React, { useEffect } from "react";
 import "./EmailRow.css";
 import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
 import { useDispatch } from "react-redux";
@@ -163,11 +7,19 @@ import { selectMail } from "./Redux/MailSlice";
 import { useNavigate } from "react-router-dom";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
-function EmailRow({ id, title1, subject, description, time, isRead }) {
+function EmailRow({ id, title1, subject, description, time }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const isRead = localStorage.getItem(`emailReadStatus:${id}`) === "true";
+
+  useEffect(() => {
+    // Store the read status in local storage when it changes
+    localStorage.setItem(`emailReadStatus:${id}`, isRead);
+  }, [id, isRead]);
+
   const openMail = () => {
+    const isRead = true; // Mark the email as read
     dispatch(
       selectMail({
         id,
@@ -175,9 +27,10 @@ function EmailRow({ id, title1, subject, description, time, isRead }) {
         subject,
         description,
         time,
-        isRead: true, // Mark the email as read when opened
+        isRead,
       })
     );
+    localStorage.setItem(`emailReadStatus:${id}`, isRead); // Store the updated read status in local storage
     navigate("/mail");
   };
 
@@ -208,4 +61,3 @@ function EmailRow({ id, title1, subject, description, time, isRead }) {
 }
 
 export default EmailRow;
-
